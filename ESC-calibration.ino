33e061c17cf6ba8752bc5f356e40579a35482bf8
lobodol <grobodol@gmail.com>
2016-01-27 16:38:13 +0100

    Update usage description

--- ESC-calibration.ino
+++ ESC-calibration.ino
@@ -1,10 +1,10 @@
 /**
  * Usage, according to documentation(http://www.hobbywing.com/uploadfiles/sx/file/manual/HW-01-V4.pdf) : 
- *     1. Power up your Arduino, open terminal, then type 1 to send max throttle to every ESC to enter programming mode
+ *     1. Plug your Arduino to your computer with USB cable, open terminal, then type 1 to send max throttle to every ESC to enter programming mode
  *     2. Power up your ESCs. You must hear "beep1 beep2 beep3" tones meaning the power supply is OK
  *     3. After 2sec, "beep beep" tone emits, meaning the throttle highest point has been correctly confirmed
  *     4. Type 0 to send 0 throttle
- *     5. Several "beep" tones emits, wich means the quantity of the lithium battery cells
+ *     5. Several "beep" tones emits, wich means the quantity of the lithium battery cells (3 beeps for a 3 cells LiPo)
  *     6. A long beep tone emits meaning the throttle lowest point has been correctly confirmed
  *     7. Type 2 to launch test function. This will send 0 to 180 throttle to ESCs to test them
  */
